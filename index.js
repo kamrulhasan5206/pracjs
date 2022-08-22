a30d3c9d03d5c9f88fe02464180964b4b98d3def
kamrulhasan5206 <95650983+kamrulhasan5206@users.noreply.github.com>
2022-08-22 17:30:56 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,64 @@
 
+"use strict";
+     
+function getidBycls(elem){
+    document.getElementById(document.querySelector(elem).id).disabled = true
+}
+
+
+
+function findPlayerNumber(playerNum){
+    if (playerNum <=5){
+        return playerNum
+    }
+    else{
+        return 5
+    }
+}
+
+
+// Counting and placing element ito selected plear list
 let count = 0;
-function myFunction(elemnt, playerName) {
+function myFunction(elemnt, playerName, cls) {
     switch(count){
         case 0:
-            var x = document.createElement(elemnt);
-            var t = document.createTextNode(playerName);
+            var x = document.createElement(elemnt)
+            var t = document.createTextNode(playerName)
             x.appendChild(t);
-            document.getElementById("myList").appendChild(x);
+            document.getElementById("myList").appendChild(x)
+            getidBycls(cls)
             count += 1 
             break
         case 1:
-            var x = document.createElement(elemnt);
-            var t = document.createTextNode(playerName);
+            var x = document.createElement(elemnt)
+            var t = document.createTextNode(playerName)
             x.appendChild(t);
-            document.getElementById("myList").appendChild(x);
+            document.getElementById("myList").appendChild(x)
+            getidBycls(cls)
             count += 1
             break 
         case 2:
-            var x = document.createElement(elemnt);
-            var t = document.createTextNode(playerName);
+            var x = document.createElement(elemnt)
+            var t = document.createTextNode(playerName)
             x.appendChild(t);
-            document.getElementById("myList").appendChild(x);
+            document.getElementById("myList").appendChild(x)
+            getidBycls(cls)
+            count += 1
+            break 
+        case 3:
+            var x = document.createElement(elemnt)
+            var t = document.createTextNode(playerName)
+            x.appendChild(t)
+            document.getElementById("myList").appendChild(x)
+            getidBycls(cls)
             count += 1
             break 
         case 4:
             var x = document.createElement(elemnt);
             var t = document.createTextNode(playerName);
             x.appendChild(t);
-            document.getElementById("myList").appendChild(x);
-            count += 1
-            break 
-        case 5:
-            var x = document.createElement(elemnt);
-            var t = document.createTextNode(playerName);
-            x.appendChild(t);
-            document.getElementById("myList").appendChild(x);
+            document.getElementById("myList").appendChild(x)
+            getidBycls(cls)
             count += 1 
             break;
         default:
@@ -43,12 +67,25 @@ function myFunction(elemnt, playerName) {
         
 
     }
-    console.log(count)
+    document.getElementById('playerNumber').value = findPlayerNumber(count)
 
+    
 }
 
 
 
 
-    
+function calculate(){
+    const persone = document.getElementById('playerNumber').value
+    const payment = document.getElementById('perPlayerCost').value
+    return parseFloat(persone).toFixed(2) * parseFloat(payment).toFixed(2)
+}
+ 
+
+const calcButton =  document.getElementById('calc')
+calcButton.addEventListener('click', function(){
+    //console.log(calculate())
+    const result = document.getElementById('result')
+    result.value = calculate()
+})
 
